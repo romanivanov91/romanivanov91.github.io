599b215a313dd19a687b836c47b83c9071330c84
romanivanov91 <92422495+romanivanov91@users.noreply.github.com>
2021-11-20 21:12:03 +0300

    Создал неработающий калькулятор)

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -1,22 +1,74 @@
 "use strict"
 
-let tipSaita = prompt ("Тип сайта. Введите порядковый номер из списка:\n1 - Интернет магазин;\n2 - Сайт услуг;\n3 - Landing Page;\n4 - Визитка;\n5 - Информационный сайт.");
-
+let selectTip = document.querySelector('select.tip');
+let selectDizain = document.querySelector('select.dizain');
+let selectAdaptivnost = document.querySelector('select.adaptivnost');
+let srok = document.querySelector('td.prav1');
+let stoimost = document.querySelector('td.prav2');
 let cenaSaita;
 let srokSaita;
+let cenaDisign;
+let srokDisign;
+let cenaAdaptivnost;
+let srokAdaptivnost;
 
-if (tipSaita == 1) {cenaSaita = 100000; srokSaita = 40
-} else if (tipSaita == 2) {cenaSaita = 50000; srokSaita = 20
-} else if (tipSaita == 3) {cenaSaita = 15000; srokSaita = 2
-} else if (tipSaita == 4) {cenaSaita = 10000; srokSaita = 2
-} else if (tipSaita == 5) {cenaSaita = 70000; srokSaita = 30
-} else {cenaSaita == "not"};
+selectTip.addEventListener('change', raschetcenasrocksaita);
 
-console.log (cenaSaita);
-console.log (srokSaita);
+function raschetcenasrocksaita() {
+	let znachvibortip = selectTip.value;	
+	if (znachvibortip === 'internetmag') {cenaSaita = 100000; srokSaita = 40
+	} else if (znachvibortip === 'saityslyg') {cenaSaita = 50000; srokSaita = 20
+	} else if (znachvibortip === 'landingpage') {cenaSaita = 15000; srokSaita = 2
+	} else if (znachvibortip === 'visitka') {cenaSaita = 10000; srokSaita = 2
+	} else if (znachvibortip === 'informsait') {cenaSaita = 70000; srokSaita = 30
+	} else {znachvibortip === "not"};
+	return cenaSaita, srokSaita
+}
 
-let disign = prompt ("Дизайн. Введите порядковый номер из списка:\n1 - Минимализм ;\n2 - Класический стиль;\n3 - Корпоративный стиль;\n4 - Рисованный стиль;\n5 - Информационный стиль.");
+selectDizain.addEventListener('change', raschetcenasrocksaitadisign);
 
+function raschetcenasrocksaitadisign() {
+	let znachvibordisign = selectDizain.value;
+
+	if (znachvibordisign === 'minimalizm') {cenaDisign = 5000; srokDisign = 1
+	} else if (znachvibordisign === 'classikstyle') {cenaDisign = 10000; srokDisign = 2
+	} else if (znachvibordisign === 'korpstyle') {cenaDisign = 15000; srokDisign = 3
+	} else if (znachvibordisign === 'risstyle') {cenaDisign = 20000; srokDisign = 10
+	} else if (znachvibordisign === 'informstyle') {cenaDisign = 25000; srokDisign = 15
+	} else {znachvibordisign === "not"};
+	return cenaDisign, srokDisign
+}
+
+selectAdaptivnost.addEventListener('change', raschetcenasrocksaitaadaptivnost);
+
+function raschetcenasrocksaitaadaptivnost() {
+	let znachviboradaptivnost = selectDizain.value;
+
+	if (znachviboradaptivnost === 'pc') {cenaAdaptivnost = 10000; srokAdaptivnost = 1
+	} else if (znachviboradaptivnost === 'mobail') {cenaAdaptivnost = 15000; srokAdaptivnost = 3
+	} else if (znachviboradaptivnost === 'pcmobail') {cenaAdaptivnost = 30000; srokAdaptivnost = 10
+	} else {znachviboradaptivnost === "not"};
+	return cenaAdaptivnost, srokAdaptivnost
+}
+
+srok.textContent = srokSaita + srokDisign + srokAdaptivnost;
+stoimost.textContent = cenaSaita + cenaDisign + cenaAdaptivnost;
+
+
+
+
+	/*console.log (cenaSaita);
+	console.log (srokSaita);
+
+
+/*if (znachvibortip === 'internetmag') {cenaSaita = 100000; srokSaita = 40
+	} else if (znachvibortip === 'saityslyg') {cenaSaita = 50000; srokSaita = 20
+	} else if (znachvibortip === 'landingpage') {cenaSaita = 15000; srokSaita = 2
+	} else if (znachvibortip === 'visitka') {cenaSaita = 10000; srokSaita = 2
+	} else if (znachvibortip === 'informsait') {cenaSaita = 70000; srokSaita = 30
+	} else {znachvibortip === "not"};
+
+/*
 let cenaDisign;
 let srokDisign;
 
@@ -30,7 +82,6 @@ if (disign == 1) {cenaDisign = 5000; srokDisign = 1
 console.log (cenaDisign);
 console.log (srokDisign);
 
-let adaptivnost = prompt ("Адаптивность. Введите порядковый номер из списка:\n1 - Адаптированность под размер дисплея персональноых компьютеров ;\n2 - Адаптированность под размеры экранов мобильных устройств;\n3 - Адаптивность под экраны всех устройств.");
 
 let cenaAdaptivnost;
 let srokAdaptivnost;
@@ -47,7 +98,6 @@ console.log (srokAdaptivnost);
 let itogCena = cenaSaita + cenaDisign + cenaAdaptivnost;
 let itogTimes = srokSaita + srokDisign + srokAdaptivnost;
  
-alert ("Ориентировочная цена разработки сайта: " + itogCena + " рублей" + "\nОриентировочный срок разработки сайта: " + itogTimes + " рабочих дней");
 
 
 /*alert (itogCena - Ориентировочная итоговая цена \n itogTimes - ориентировочный срок разработки сайта)*/
