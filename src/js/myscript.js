654e99528953d8901705eb645a23d2a3854aa12b
romanivanov91 <92422495+romanivanov91@users.noreply.github.com>
2021-11-22 07:35:28 +0300

    Неработающий калькулятор + бегущие цифры

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -1,5 +1,56 @@
 "use strict"
 
+/*
+$(document).ready (function() {
+
+	let options = {threshold: [0.5]};
+	let observer = new IntersectionObserver(onEntry, options);
+	let elements = $('.elementanimation')
+	elements.each ((i, el) => {
+		observer.observe (el);
+	})
+	$('.elementanimation').css('opacity', '1');
+	$('.elementanimation').spincrement({
+        thousandSeparator: "",
+        duration: 1200
+    });
+
+})
+
+function onEntry (entry) {
+	entry.forEach (change => {
+		if (change.isIntersecting) {
+			change.target.classList.add ('show-animation');
+		}
+	})
+}
+*/
+
+$(document).ready(function () {
+
+    var show = true;
+    var countbox = ".statistics";
+    $(window).on("scroll load resize", function () {
+        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+        var w_height = $(window).height(); // Высота окна браузера
+        var d_height = $(document).height(); // Высота всего документа
+        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+            $('.elementanimation').css('opacity', '1');
+            $('.elementanimation').spincrement({
+                thousandSeparator: "",
+                duration: 5000
+            });
+
+            show = false;
+        }
+    });
+
+});
+
+
 let selectTip = document.querySelector('select.tip');
 let selectDizain = document.querySelector('select.dizain');
 let selectAdaptivnost = document.querySelector('select.adaptivnost');
@@ -12,47 +63,62 @@ let srokDisign;
 let cenaAdaptivnost;
 let srokAdaptivnost;
 
-selectTip.addEventListener('change', raschetcenasrocksaita);
+selectTip.addEventListener('change', raschetcenasrocksaita());
 
-function raschetcenasrocksaita() {
+function raschetcenasrocksaita() {	
 	let znachvibortip = selectTip.value;	
-	if (znachvibortip === 'internetmag') {cenaSaita = 100000; srokSaita = 40
-	} else if (znachvibortip === 'saityslyg') {cenaSaita = 50000; srokSaita = 20
-	} else if (znachvibortip === 'landingpage') {cenaSaita = 15000; srokSaita = 2
-	} else if (znachvibortip === 'visitka') {cenaSaita = 10000; srokSaita = 2
-	} else if (znachvibortip === 'informsait') {cenaSaita = 70000; srokSaita = 30
-	} else {znachvibortip === "not"};
-	return cenaSaita, srokSaita
+	if (znachvibortip == 'internetmag') {cenaSaita = 100000; srokSaita = 40
+	} else if (znachvibortip == 'saityslyg') {cenaSaita = 50000; srokSaita = 20
+	} else if (znachvibortip == 'landingpage') {cenaSaita = 15000; srokSaita = 2
+	} else if (znachvibortip == 'visitka') {cenaSaita = 10000; srokSaita = 2
+	} else if (znachvibortip == 'informsait') {cenaSaita = 70000; srokSaita = 30
+	} else {znachvibortip == "not"};
+	/*return cenaSaita, srokSaita*/
 }
 
-selectDizain.addEventListener('change', raschetcenasrocksaitadisign);
+console.log (cenaSaita);
+console.log (srokSaita);
+
+selectDizain.addEventListener('change', raschetcenasrocksaitadisign());
 
 function raschetcenasrocksaitadisign() {
 	let znachvibordisign = selectDizain.value;
 
-	if (znachvibordisign === 'minimalizm') {cenaDisign = 5000; srokDisign = 1
-	} else if (znachvibordisign === 'classikstyle') {cenaDisign = 10000; srokDisign = 2
-	} else if (znachvibordisign === 'korpstyle') {cenaDisign = 15000; srokDisign = 3
-	} else if (znachvibordisign === 'risstyle') {cenaDisign = 20000; srokDisign = 10
-	} else if (znachvibordisign === 'informstyle') {cenaDisign = 25000; srokDisign = 15
-	} else {znachvibordisign === "not"};
-	return cenaDisign, srokDisign
+	if (znachvibordisign == 'minimalizm') {cenaDisign = 5000; srokDisign = 1
+	} else if (znachvibordisign == 'classikstyle') {cenaDisign = 10000; srokDisign = 2
+	} else if (znachvibordisign == 'korpstyle') {cenaDisign = 15000; srokDisign = 3
+	} else if (znachvibordisign == 'risstyle') {cenaDisign = 20000; srokDisign = 10
+	} else if (znachvibordisign == 'informstyle') {cenaDisign = 25000; srokDisign = 15
+	} else {znachvibordisign == "not"};
+	/*return cenaDisign, srokDisign*/
 }
 
-selectAdaptivnost.addEventListener('change', raschetcenasrocksaitaadaptivnost);
+selectAdaptivnost.addEventListener('change', raschetcenasrocksaitaadaptivnost());
 
 function raschetcenasrocksaitaadaptivnost() {
-	let znachviboradaptivnost = selectDizain.value;
+	let znachviboradaptivnost = selectAdaptivnost.value;
 
-	if (znachviboradaptivnost === 'pc') {cenaAdaptivnost = 10000; srokAdaptivnost = 1
-	} else if (znachviboradaptivnost === 'mobail') {cenaAdaptivnost = 15000; srokAdaptivnost = 3
-	} else if (znachviboradaptivnost === 'pcmobail') {cenaAdaptivnost = 30000; srokAdaptivnost = 10
-	} else {znachviboradaptivnost === "not"};
-	return cenaAdaptivnost, srokAdaptivnost
+	if (znachviboradaptivnost == 'pc') {cenaAdaptivnost = 10000; srokAdaptivnost = 1
+	} else if (znachviboradaptivnost == 'mobail') {cenaAdaptivnost = 15000; srokAdaptivnost = 3
+	} else if (znachviboradaptivnost == 'pcmobail') {cenaAdaptivnost = 30000; srokAdaptivnost = 10
+	} else {znachviboradaptivnost == "not"};
+	/*return cenaAdaptivnost, srokAdaptivnost*/
 }
+
+
+console.log (cenaDisign);
+console.log (srokDisign);
+console.log (cenaAdaptivnost);
+console.log (srokAdaptivnost);
+
 
 srok.textContent = srokSaita + srokDisign + srokAdaptivnost;
 stoimost.textContent = cenaSaita + cenaDisign + cenaAdaptivnost;
+
+$('a[href^="#"]').click (function() {
+	let valHref = $(this).attr("href");
+	$('html, body').animate({scrollTop: $(valHref).offset().top - 60 + "px"})
+})
 
 
 
